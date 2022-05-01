8ee00e74bd54d68caa1baaa415bcc7337b45e4f5
chrisrca <goalie.cyclone@gmail.com>
2022-05-01 01:33:09 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron');
+const { API, Regions, Locales, Queue } = require("node-valorant-api");
+
+const APIKey = "~~~~~-~~~~~~~~-~~~~-~~~~-~~~~-~~~~~~~~~~~~";
+const valorant = new API(Regions.NA, APIKey, Regions.AMERICAS);
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 920, height: 300,  frame:false, transparent:true, backgroundColor: '#00FFFFFF'});
+  mainWindow.loadFile('main.html');
+  mainWindow.setMenu(null)
+  mainWindow.setMinimizable(false)
+  mainWindow.maximize()
+  mainWindow.setIgnoreMouseEvents(true)
+  mainWindow.setAlwaysOnTop(true, 'screen')
+  mainWindow.focus(true)
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+function printData() {
+  valorant.ContentV1.getContent(Locales["en-US"]).then(content => {
+    console.log(content.characters.map(char => { return char.name }));
+});
+
+  valorant.AccountV1.getAccountByRiotID("~~~~", "~~~~").then(account => {
+    valorant.MatchV1.getMatchesByPuuid(account.puuid).then(matches => {
+        console.log(matches);
+    })
+  });
+}
+
+app.on('ready', () => setTimeout(createWindow, 400), printData());
