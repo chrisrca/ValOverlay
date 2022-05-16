650ea82f4d5281a15d8b3bf281a697fea62dd75a
chrisrca <goalie.cyclone@gmail.com>
2022-05-15 23:52:11 -0400

    prep for accessing binds, crosshair, etc

---
+++ clientinfo/clientdata.js
@@ -0,0 +1,12 @@
+// first get text from
+//C:\Users\goali\AppData\Local\VALORANT\Saved\Config\Windows\RiotLocalMachine.ini
+
+// Parse: 
+/* 
+[UserInfo]
+LastKnownUser=cf8c8f23-212e-5ac2-ab15-856455217453
+*/
+// add "-region" to the end 
+// ex: "-na"
+// get from rso endpoint
+
