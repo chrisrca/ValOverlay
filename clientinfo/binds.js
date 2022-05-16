adad29cb31a26b99b395a86fbc3825a2d04bb7b2
chrisrca <goalie.cyclone@gmail.com>
2022-05-15 23:51:41 -0400

    prep for binds (needed for round buy tracking)

---
+++ clientinfo/binds.js
@@ -0,0 +1,31 @@
+/* BackupKeybinds.json found in C:\Users\goali\AppData\Local\VALORANT\Saved\Config\cf8c8f23-212e-5ac2-ab15-856455217453-na\WindowsClient
+
+{
+	"settingsVersion": 13,
+	"actionMappings": [
+		{
+			"name": "Spray",
+			"characterName": "None",
+			"bindIndex": 0,
+			"key": "Y",
+			"shift": false,
+			"ctrl": false,
+			"alt": false,
+			"cmd": false
+		},
+		{
+			"name": "Inspect",
+			"characterName": "None",
+			"bindIndex": 0,
+			"key": "T",
+			"shift": false,
+			"ctrl": false,
+			"alt": false,
+			"cmd": false
+		}
+	],
+	"axisMappings": [],
+	"settingsProfiles": []
+}
+
+*/
