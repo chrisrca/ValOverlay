16fa0537a5a74b750fb71d97d656f6694bb20097
Christian-Reynolds-2022 <christianreynolds22@bghs.org>
2022-05-17 08:42:40 -0400

    set rso fetcher to an exportable function

--- clientinfo/username.js
+++ clientinfo/username.js
@@ -1,44 +1,12 @@
-import base64 from 'base-64';
-import fetch from 'node-fetch';
-import fs from 'fs';
+import {getUserInfo} from './rsofetcher.js'
 
-// Call with:
-let name 
-try {name = await getUserInfo()} 
-    catch (err) // Catch errors
+let lockDataJson 
+let username
+try {lockDataJson = await getUserInfo()
+     username = [lockDataJson.acct.game_name, lockDataJson.acct.tag_line] 
+    } 
+    catch (err) 
     {console.log('Lockfile does not yet exist or rso is not avalible')}
 
-console.log(name)
+console.log(username)
 
-async function getUserInfo () {
-    // Set path and disable TLS certification
-    let lockFilePath = process.env.LOCALAPPDATA + '\\Riot Games\\Riot Client\\Config\\lockfile'
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-    // Read lockFile
-    let lockFileData = await readLockData()
-    function readLockData(){        
-        return fs.readFileSync(lockFilePath).toString()
-    }
-
-    // Split lockfile into array
-    let userInfo = lockFileData.split(':')
-
-    // Create url and credentials using lockfile array
-    let url = (`https://127.0.0.1:${userInfo[2]}/rso-auth/v1/authorization/userinfo`)
-    let credentials = base64.encode(`riot:${userInfo[3]}`)
-
-    // Fetch user info from local api
-    let rawuserinfo
-    await fetch(url, {method:'GET', headers: {'Authorization': `Basic ${credentials}`       
-    }})
-        .then(response => response.json())
-        .then(json => rawuserinfo = json);
-
-    // Parse userinfo    
-    rawuserinfo = JSON.parse(rawuserinfo.userInfo)
-
-    // Return name and tagline
-    let username = [rawuserinfo.acct.game_name, rawuserinfo.acct.tag_line] 
-    return (username)
-}
